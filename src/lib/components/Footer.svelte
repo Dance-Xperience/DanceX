5e590bb52d0126302efa278c31ebcc7139dccaf3
mia-cx <mia@mia.cx>
2023-01-16 04:09:11 +0100

    start with footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,25 @@
+<script>
+	import id from '../../identity';
+</script>
+
+<footer id="footer" class="static lg:absolute lg:bottom-0 flex justify-center items-center w-full">
+        <!-- TODO light vs dark theme -->
+	<div
+		id="footer__content"
+		class="
+                        flex flex-col flex-grow justify-between items-center 
+                        max-w-7xl
+                        bg-gray-800 lg:bg-white 
+                        text-white lg:text-black
+                "
+	>
+		<!-- border -->
+		<div class="w-0 transition-all lg:w-[92%] max-w-7xl mt-2 border-t-2 border-black" />
+
+		
+
+		Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat incidunt, odit illum molestiae
+		quisquam esse similique, deleniti dolores earum excepturi libero repellat soluta reprehenderit aspernatur
+		rem asperiores nam dolor ex.
+	</div>
+</footer>
