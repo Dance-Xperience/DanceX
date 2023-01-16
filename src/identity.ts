9cca320cc0aa78d7d7ae8170ac939c65d616d4bc
Yaraka <55541237+Sorasuo@users.noreply.github.com>
2023-01-16 09:05:16 +0100

    Update identity.ts

--- src/identity.ts
+++ src/identity.ts
@@ -1,7 +1,7 @@
 const id = {
 	name: 'Dance Xperience',
 	description:
-		'Luister elke donderdag avond LIVE naar DanceXperience van 19:00 - 00:00 op Omroep XON. FM 107,3 MHz | DAB+ Kanaal 5A (174,928 MHz)',
+		'Luister elke donderdag avond LIVE naar DanceXperience van 19:00 - 00:00 op Ede FM. FM 107,3 MHz | DAB+ Kanaal 5A (174,928 MHz)',
 	tagline: '',
 	url: 'https://dancex.nl/',
 	keywords: ['radio', 'omroep'],
