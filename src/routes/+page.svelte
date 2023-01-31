5f8043a69a9c49fb1e0e4fe5ab41b08fde265217
mia-cx <mia@mia.cx>
2023-02-01 00:28:07 +0100

    margin to improve a11y on mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,28 +2,35 @@
 	import '../app.css';
 </script>
 
-<h1>Dance Xperience</h1>
-<p>
-	Dance Xperience gaat weer LIVE vanaf 2 februari! We hopen jullie allemaal te zien bij ons nieuwe
-	thuis, Ede FM.
-</p>
-<p>
-	De uitzendtijd zal blijven zoals jullie van ons gewend zijn:<br /> LIVE in de studio van 19:00 - 21:00
-	met DJ Kessie en Sidekick Joshua en daarna van 21:00 - 00:00 geprogrammeerde mixen!
-</p>
-<p>
-	Je kunt naar ons luisteren via de FM op 107.3 MHz of via de online stream op
-	<a href="https://omroepede.nl/radio">https://omroepede.nl/radio</a>.
-</p>
-<p>
-	Deze website is momenteel onder constructie ter voorbereiding van de verhuizing van radio
-	stations.<br /> We proberen de website zo snel mogelijk weer live te krijgen.
-</p>
-<p>
-	Volg voor meer informatie <a href="https://www.facebook.com/djkessie/">Ap Kessie op Facebook!</a>
-</p>
+<main>
+	<h1>Dance Xperience</h1>
+	<p>
+		Dance Xperience gaat weer LIVE vanaf 2 februari! We hopen jullie allemaal te zien bij ons nieuwe
+		thuis, Ede FM.
+	</p>
+	<p>
+		De uitzendtijd zal blijven zoals jullie van ons gewend zijn:<br /> LIVE in de studio van 19:00 -
+		21:00 met DJ Kessie en Sidekick Joshua en daarna van 21:00 - 00:00 geprogrammeerde mixen!
+	</p>
+	<p>
+		Je kunt naar ons luisteren via de FM op 107.3 MHz of via de online stream op
+		<a href="https://omroepede.nl/radio">https://omroepede.nl/radio</a>.
+	</p>
+	<p>
+		Deze website is momenteel onder constructie ter voorbereiding van de verhuizing van radio
+		stations.<br /> We proberen de website zo snel mogelijk weer live te krijgen.
+	</p>
+	<p>
+		Volg voor meer informatie <a href="https://www.facebook.com/djkessie/">Ap Kessie op Facebook!</a
+		>
+	</p>
+</main>
 
 <style>
+	main {
+		margin: 4em;
+		max-width: 1280px;
+	}
 	* {
 		text-align: center;
 	}
