a7616306c5ee8fc3d228f0c6df35adb0fcf708c8
Mia <mia@mia.cx>
2023-03-30 22:35:35 +0200

    update placeholder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import '../app.css';
 </script>
 
@@ -8,24 +8,41 @@
 	<meta name="keywords" content="Dance Xperience, Omroep Ede, Ede FM, Dance, Radio" />
 </svelte:head>
 
-<main>
-	<h1>Dance Xperience</h1>
-	<p>
-		Dance Xperience gaat weer LIVE vanaf 2 februari! We hopen jullie allemaal te zien bij ons nieuwe
-		thuis, Ede FM.
-	</p>
-	<p>
-		De uitzendtijd zal blijven zoals jullie van ons gewend zijn:<br /> LIVE in de studio van 19:00 -
-		21:00 met DJ Kessie en Sidekick Joshua en daarna van 21:00 - 00:00 geprogrammeerde mixen!
-	</p>
-	<p>
-		Je kunt naar ons luisteren via de FM op 107.3 MHz of via de online stream op
-		<a href="https://omroepede.nl/radio">https://omroepede.nl/radio</a>.
-	</p>
-	<p>
-		Deze website is momenteel onder constructie ter voorbereiding van de verhuizing van radio
-		stations.<br /> We proberen de website zo snel mogelijk weer live te krijgen.
-	</p>
+<main class="text-left">
+        <h1>Dance Xperience</h1>
+	<p>Deze website is momenteel onder constructie.</p>
+	<p>We proberen de website zo snel mogelijk weer live te krijgen, excuses voor het ongemak.</p>
+	<p>Dance Xperience is een radio show gepresenteerd door host DJ Kessie en sidekick DJ Joshua.</p>
+	<p>Ons programma is te beluisteren op radio station Ede FM.</p>
+
+	<h2>Programmering:</h2>
+	<ul>
+		<li>19:00 - 21:00 - Live presentatie</li>
+		<li>21:00 - 00:00 - Ingeprogrammeerde thema mixen</li>
+	</ul>
+
+	<p>We zijn te beluisteren via de volgende kanalen:</p>
+	<ul>
+		<li>FM - 107.3 MHz</li>
+		<li>DAB+ - Kanaal 5A (174,928 MHz)</li>
+		<li>Online stream - <a href="https://omroepede.nl/radio">https://omroepede.nl/radio</a></li>
+	</ul>
+
+	<h2>Contact</h2>
+	<ul>
+		<li>
+			<a href="mailto:info@dancex.nl">info@dancex.nl</a> - Voor het stellen van vragen of verzoekjes
+		</li>
+		<li>
+			<a href="mailto:aanmelden@dancex.nl">aanmelden@dancex.nl</a> - Als je je wilt aanmelden als gast
+			bij ons in het programma
+		</li>
+		<li>
+			<a href="mailto:mix@dancex.nl">mix@dancex.nl</a> - Voor het aanleveren van mixen (download link
+			naar mix in de mail, liefst in .wav formaat)
+		</li>
+	</ul>
+
 	<p>
 		Volg voor meer informatie <a href="https://www.facebook.com/djkessie/">Ap Kessie op Facebook!</a
 		>
